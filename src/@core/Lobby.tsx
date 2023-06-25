9aab871493ec3157d8befb16a91b499b57baf35e
jrmncos <costillagermanalfredo@gmail.com>
2023-06-25 00:46:20 -0300

    fixing conflicts

--- src/@core/Lobby.tsx
+++ src/@core/Lobby.tsx
@@ -1,140 +1,139 @@
-import React, { useState, CSSProperties } from 'react';
-import { Redirect } from 'react-router-dom';
-import { Col, Row, Button, Container } from 'react-bootstrap';
+import React, { useState, CSSProperties, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import { Col, Row, Button, Container } from "react-bootstrap";
 /** @jsx jsx */
-import { jsx, css}  from '@emotion/core';
-import game from '../styles/global';
+import { jsx, css } from "@emotion/core";
 
-import JoinedPlayers from './JoinedPlayers';
-import useSocket from './socket/useSocket';
-import MechaRPGLogic from '../components/MechaRPGLogic';
-import { Mecha, Player, InitState, Turn } from './logic/GameState';
-import GameContainer from '../components/GameContainer';
+import JoinedPlayers from "./JoinedPlayers";
+import useSocket from "./socket/useSocket";
+
+import { Turn } from "./logic/GameState";
+import GameContainer from "../components/GameContainer";
 
 interface LobbyProps {
-    roomId: string;
-    password: string;
+  roomId: string;
+  password: string;
 }
 
 export type PlayerJoined = {
-    id: string;
-    isReady: boolean;
-    username: string;
+  id: string;
+  isReady: boolean;
+  username: string;
 };
 
 export default function Lobby({ roomId, password }: LobbyProps) {
-    const { emit, subscribeTo } = useSocket();
-    const [isReady, setIsReady] = useState(false);
-    const [playersJoined, setPlayerJoined] = useState<PlayerJoined[]>([]);
-    // Is ready to start the game
-    const [gameIsReady, setGameIsReady] = useState(false);
-    // Actual turn
-    const [turn, setTurn] = useState<Turn>();
-    const [sentTurn, setSentTurn] = useState(false);
-    // Actual player has the turn
-    const [isTurn, setIsTurn] = useState(false);
+  const { emit, subscribeTo } = useSocket();
+  const [isReady, setIsReady] = useState(false);
+  const [playersJoined, setPlayerJoined] = useState<PlayerJoined[]>([]);
+  // Is ready to start the game
+  const [gameIsReady, setGameIsReady] = useState(false);
+  // Actual turn
+  const [turn, setTurn] = useState<Turn>();
+  const [sentTurn, setSentTurn] = useState(false);
+  // Actual player has the turn
+  const [isTurn, setIsTurn] = useState(false);
 
-    subscribeTo.personalTurnStart(() => {
-        setIsTurn(true);
-    });
+  subscribeTo.personalTurnStart(() => {
+    setIsTurn(true);
+  });
 
-    subscribeTo.personalTurnEnd(() => {
-        setIsTurn(false);
-        setTurn(null);
-        setSentTurn(false);
-    });
+  subscribeTo.personalTurnEnd(() => {
+    setIsTurn(false);
+    setTurn(null);
+    setSentTurn(false);
+  });
 
-    // Review this...
-    useEffect(() => {
-        const handleDraftStart = () => {
-            setGameIsReady(true);
-        };
-        subscribeTo.draftStart(handleDraftStart);
-    }, [playersJoined]);
-
-    //
-    useEffect(() => {
-        if (sentTurn && turn) {
-            emit.playerTurnPass(turn);
-        }
-    }, [turn, sentTurn]);
-
-    const PreDraft = () => {
-        const message = isReady ? 'WAITING FOR OTHERS...' : 'YOU READY?';
-        return (
-            <Button
-                style={{ fontSize: 32, margin: 4 }}
-                disabled={isReady}
-                onClick={() => {
-                    emit.playerReady();
-                    setIsReady(true);
-                }}
-            >
-                {message}
-            </Button>
-        );
+  // Review this...
+  useEffect(() => {
+    const handleDraftStart = () => {
+      setGameIsReady(true);
     };
+    subscribeTo.draftStart(handleDraftStart);
+  }, [playersJoined]);
 
-    const handleEndTurn = event => {
-        event.preventDefault();
-        setSentTurn(true);
-    };
+  //
+  useEffect(() => {
+    if (sentTurn && turn) {
+      emit.playerTurnPass(turn);
+    }
+  }, [turn, sentTurn]);
 
-    const OnDraft = () => {
-        return (
-            <div>
-                <Button
-                    style={{ fontSize: 16, margin: 4 }}
-                    disabled={!isTurn}
-                    onClick={handleEndTurn}
-                >
-                    <div>
+  const PreDraft = () => {
+    const message = isReady ? "WAITING FOR OTHERS..." : "YOU READY?";
+    return (
+      <Button
+        style={{ fontSize: 32, margin: 4 }}
+        disabled={isReady}
+        onClick={() => {
+          emit.playerReady();
+          setIsReady(true);
+        }}
+      >
+        {message}
+      </Button>
+    );
+  };
+
+  const handleEndTurn = event => {
+    event.preventDefault();
+    setSentTurn(true);
+  };
+
+  const OnDraft = () => {
+    return (
+      <div>
+        <Button
+          style={{ fontSize: 16, margin: 4 }}
+          disabled={!isTurn}
+          onClick={handleEndTurn}
+        >
+          <div>
                         <span style={{ fontSize: 24 }}>
                             END TURN <br />
                         </span>
-                    </div>
-                </Button>
-            </div>
-        );
-    };
-
-    const updatePlayers = psJoined => {
-        setPlayerJoined(psJoined);
-    };
-
-    return (
-        <div>
-            <div>
-                {gameIsReady ? (
-                  <GameContainer
-                    playersJoined={playersJoined}
-                    isTurn={isTurn}
-                    setTurn={setTurn}
-                    sentTurn={sentTurn}
-                  />
-                ) : (
-                  <Button style={{ fontSize: 32, margin: 4 }} disabled>
-                      Game is not started
-                  </Button>
-                )}
-            </div>
-                <div style={turnMenu}>
-                    <JoinedPlayers
-                        playersJoined={playersJoined}
-                        onPlayerJoin={updatePlayers}
-                    />
-                    {!isReady && <PreDraft />}
-                    {isReady && <OnDraft />}
-            </div>
-        </div>
+          </div>
+        </Button>
+      </div>
     );
+  };
+
+  const updatePlayers = psJoined => {
+    setPlayerJoined(psJoined);
+  };
+
+  return (
+    <div>
+      <div>
+        {gameIsReady ? (
+          <GameContainer
+            playersJoined={playersJoined}
+            isTurn={isTurn}
+            setTurn={setTurn}
+            sentTurn={sentTurn}
+          />
+        ) : (
+          <Button style={{ fontSize: 32, margin: 4 }} disabled>
+            Game is not started
+          </Button>
+        )}
+      </div>
+      <div style={turnMenu}>
+        <JoinedPlayers
+          playersJoined={playersJoined}
+          onPlayerJoin={updatePlayers}
+        />
+        {!isReady && <PreDraft />}
+        {isReady && <OnDraft />}
+      </div>
+    </div>
+  );
 }
 
 const turnMenu: CSSProperties = {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '20%',
-    height: '20%',
-    zIndex: 1,
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "20%",
+  height: "20%",
+  zIndex: 1
 };
--- src/components/GameContainer.tsx
+++ src/components/GameContainer.tsx
@@ -1,109 +1,113 @@
-import { css, Global } from '@emotion/core';
-import globalStyles from '../styles/global';
-import React, { Dispatch, SetStateAction, useState } from 'react';
-import Game from '../@core/Game';
-import useWindowSize from '../@core/useWindowSize';
-import MechaRPGLogic from './MechaRPGLogic';
-import { InitState, Mecha, Player, Turn } from '../@core/logic/GameState';
-import { PlayerJoined } from '../@core/Lobby';
-import useAuth from '../@core/auth/useAuth';
-import useSocket from '../@core/socket/useSocket';
+import { css, Global } from "@emotion/core";
+import React, { Dispatch, SetStateAction, useState } from "react";
+
+import globalStyles from "../styles/global";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import MechaRPGLogic from "./MechaRPGLogic";
+import { InitState, Mecha, Player, Turn } from "../@core/logic/GameState";
+import useAuth from "../@core/auth/useAuth";
+import useSocket from "../@core/socket/useSocket";
+import { PlayerJoined } from "../@core/Lobby";
+import useWindowSize from "../@core/useWindowSize";
+import Game from "../@core/Game";
 
 const styles = {
-    root: (width: number, height: number) => css`
-        display: flex;
-        width: ${width - (width % 2)}px;
-        height: ${height - (height % 2)}px;
-        justify-content: center;
-        align-items: center;
-    `,
+  root: (width: number, height: number) => css`
+    display: flex;
+    width: ${width - (width % 2)}px;
+    height: ${height - (height % 2)}px;
+    justify-content: center;
+    align-items: center;
+  `
 };
 
 export interface GameContainerProps {
-    isTurn: boolean;
-    playersJoined: PlayerJoined[];
-    setTurn: Dispatch<SetStateAction<Turn>>;
-    sentTurn: boolean;
+  isTurn: boolean;
+  playersJoined: PlayerJoined[];
+  setTurn: Dispatch<SetStateAction<Turn>>;
+  sentTurn: boolean;
 }
 
 export default function GameContainer({
-    isTurn,
-    playersJoined,
-    setTurn,
-    sentTurn,
-}: GameContainerProps) {
-    const { username } = useAuth();
-    const [width, height] = useWindowSize();
-    const { subscribeTo } = useSocket();
-    // Turns for all players...
-    const [incomingTurn, setIncomingTurn] = useState<Turn>();
+                                        isTurn,
+                                        playersJoined,
+                                        setTurn,
+                                        sentTurn
+                                      }: GameContainerProps) {
+  const { walletData } = useAuth();
+  const [width, height] = useWindowSize();
+  const { subscribeTo } = useSocket();
+  // Turns for all players...
+  const [incomingTurn, setIncomingTurn] = useState<Turn>();
 
-    subscribeTo.showTurns((err, iTurn) => {
-        console.log('Incoming turn', iTurn);
-        setIncomingTurn((iTurn as unknown) as Turn);
-    });
+  subscribeTo.showTurns((err, iTurn) => {
+    console.log("Incoming turn", iTurn);
+    setIncomingTurn((iTurn as unknown) as Turn);
+  });
 
-    const initState = () => {
-        // TODO: Crear los mechas y el init state a partir de los players joined.
-        console.log(playersJoined);
-        const mecha1: Mecha = ({
-            idOwner: 'Santieight',
-            id: '87',
-            position: { x: 10, y: 6 },
-            hp: 200,
-            hpTotal: 200,
-            attack: 20,
-            armor: 5,
-            isReady: false,
-        } as unknown) as Mecha;
+  const initState = () => {
+    // TODO: Crear los mechas y el init state a partir de los players joined.
+    console.log(playersJoined);
+    const mecha1: Mecha = ({
+      idOwner: "0x20E2Ff1b50Ae6c18B7f2E88423dCd76bCEAf83b1aba43b0C3DCDF0Ae4Ae280a",
+      id: "87",
+      position: { x: 10, y: 6 },
+      hp: 200,
+      hpTotal: 200,
+      attack: 20,
+      armor: 5,
+      isReady: false
+    } as unknown) as Mecha;
 
-        const mecha2: Mecha = ({
-            idOwner: 'Damistone',
-            id: '73',
-            position: { x: 16, y: 8 },
-            hp: 150,
-            hpTotal: 150,
-            attack: 10,
-            armor: 5,
-            isReady: false,
-        } as unknown) as Mecha;
+    const mecha2: Mecha = ({
+      idOwner: "Damistone",
+      id: "73",
+      position: { x: 16, y: 8 },
+      hp: 150,
+      hpTotal: 150,
+      attack: 10,
+      armor: 5,
+      isReady: false
+    } as unknown) as Mecha;
 
-        const players: Player[] = [
-            ({
-                id: 1997,
-                username: 'Santieight',
-            } as unknown) as Player,
-            ({
-                id: 1995,
-                username: 'Damistone',
-            } as unknown) as Player,
-        ];
+    const players: Player[] = [
+      ({
+        id: 1997,
+        username: "Santieight"
+      } as unknown) as Player,
+      ({
+        id: 1995,
+        username: "Damistone"
+      } as unknown) as Player
+    ];
 
-        players[0].mechas = [mecha1];
-        players[1].mechas = [mecha2];
+    players[0].mechas = [mecha1];
+    players[1].mechas = [mecha2];
 
-        const state = ({
-            players,
-        } as unknown) as InitState;
+    const state = ({
+      players
+    } as unknown) as InitState;
 
-        return state;
-    };
+    return state;
+  };
 
-    return (
-        <>
-            <Global styles={globalStyles} />
-            <div css={styles.root(width, height)}>
-                <Game cameraZoom={80}>
-                    <MechaRPGLogic
-                        initState={initState()}
-                        isTurn={isTurn}
-                        username={username}
-                        sentTurn={sentTurn}
-                        setTurn={setTurn}
-                        incomingTurn={incomingTurn}
-                    />
-                </Game>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Global styles={globalStyles} />
+      <div css={styles.root(width, height)}>
+        <Game cameraZoom={80}>
+          <MechaRPGLogic
+            initState={initState()}
+            isTurn={isTurn}
+            walletData={walletData}
+            sentTurn={sentTurn}
+            setTurn={setTurn}
+            incomingTurn={incomingTurn}
+          />
+        </Game>
+      </div>
+    </>
+  );
+
 }
